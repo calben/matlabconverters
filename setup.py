58feee624f48faa49e96848e7bdaecdfc8190606
Calem Bendell <calem.j.bendell@gmail.com>
2016-01-19 17:22:40 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='MatLabConverters',
+        version='0.1',
+        packages=['matlabconverters'],
+        url='',
+        license='MIT',
+        author='Calem Bendell',
+        author_email='calem.j.bendell@gmail.com',
+        description=''
+)
